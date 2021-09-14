aebb572a119121103a36758e36531b609382f282
Juan Fernando Rios <juan.rios27@udea.edu.co>
2021-09-13 20:09:19 -0500

    Diseño en la vista de lista de productos

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SimpleProduct } from '../interfaces/productInterfaces';
+import currencyFormatter from 'currency-formatter';
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -16,23 +17,35 @@ export const ProductCard = ({ product, onClick }: Props) => {
     return (
         <TouchableOpacity
             onPress={onClick.bind(this, product.id)}
-            activeOpacity={0.9}>
-            <View style={{ ...styles.cardContainter }}>
+            activeOpacity={0.9}
+            style={{ backgroundColor: 'white' }}>
+            <View style={{
+                ...styles.cardContainer,
+                width: windowWidth * 0.4,
+            }}>
+                <View style={styles.cardContent}>
+                    <View style={styles.imageContainer}>
+                        <Image
+                            source={{ uri: 'https://http2.mlstatic.com/D_684803-MLA44155935502_112020-O.jpg' }}
+                            style={styles.image}
+                            resizeMode='contain'
+                        />
+                    </View>
+                    <View style={styles.detailContainer}>
 
-                <View>
-                    <Image
-                        source={{ uri: product.urlFoto }}
-                        style={styles.image}
-                    />
+                        <View style={styles.detailContent} >
+
+                            <Text style={styles.name}> {product.nombre} </Text>
+                            <Text style={styles.priceAndBrand}> Apple </Text>
+                            <Text style={styles.priceAndBrand}> {currencyFormatter.format(product.precio, { code: 'COP', precision: 0 })}</Text>
+                        </View>
+                        <TouchableOpacity style={styles.addCartButton}>
+                            <Text style={styles.textBuyButton}>
+                                Agregar al carrito
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
-                <View style={{ width: 100 }} >
-
-                    <Text style={styles.name}> {product.nombre} </Text>
-                    {/* <Text style={styles.brand}> {'\n' + product.marca}</Text> */}
-                    <Text style={styles.price}> {'\n' + product.precio}</Text>
-                </View>
-
-
             </View>
 
         </TouchableOpacity>
@@ -40,29 +53,59 @@ export const ProductCard = ({ product, onClick }: Props) => {
 }
 
 const styles = StyleSheet.create({
-    cardContainter: {
-        marginHorizontal: 20,
-        borderColor: 'black',
-        borderWidth: 1,
-        height: 250,
-        width: 140,
-        marginBottom: 25,
+    cardContainer: {
+        marginHorizontal: 10,
+        height: 300,
+        width: 160,
         borderRadius: 5,
-        alignItems: 'center',
+        marginBottom: 25,
+        shadowColor: "#000",
+        shadowRadius: 7,
+        elevation: 1,
+
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+    },
+    cardContent: {
         flex: 1,
-        textAlign: 'center'
+        flexDirection: 'column',
+        backgroundColor: '#F7F7F7',
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5
     },
     name: {
-        color: '#3B3B3B',
-        //fontFamily: ' Roboto', 
+        color: '#5C5E64',
         fontSize: 13,
-        fontWeight: 'normal',
-        position: 'absolute',
-        top: 200,
-
+        fontWeight: 'bold',
+        textAlign: "center",
+        paddingTop: 10
+    },
+    imageContainer: {
+        width: '100%',
+        height: '58%',
+        marginTop: 1,
+        flex: 1,
+        overflow: 'hidden',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        position: 'relative',
+    },
+    detailContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    detailContent: {
+        height: '100%',
+        paddingHorizontal: 20,
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between'
     },
     brand: {
-        //  fontFamily: ' Roboto', 
         fontSize: 12,
         color: '#772CE8',
         fontWeight: '600',
@@ -72,19 +115,33 @@ const styles = StyleSheet.create({
 
     },
     image: {
-        width: 120,
-        height: 200,
-        position: 'absolute',
-        bottom: -200,
-        right: -60,
-        borderRadius: 5
+        width: '100%',
+        height: '100%',
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+        flex: 1
     },
-    price: {
-        // fontFamily: ' Roboto', 
+    priceAndBrand: {
         fontWeight: 'bold',
         fontSize: 14,
         color: '#772CE8',
-        top: 210
+        textAlign: "center"
+    },
+    addCartButton: {
+        backgroundColor: '#7444FB',
+        paddingVertical: 6,
+        borderRadius: 20,
+        paddingHorizontal: 25,
+        shadowColor: "#000",
+        shadowOpacity: 0.24,
+        shadowRadius: 7,
+        elevation: 4,
+        marginVertical: 15
+    },
+    textBuyButton: {
+        color: '#fff',
+        fontSize: 10,
+        fontWeight: 'bold'
     }
 
 })
