59c1ed1e0e34f3988852e0858f14e812a6313f94
Juan Fernando Rios <juan.rios27@udea.edu.co>
2021-09-22 19:21:53 -0500

    Interfaz para el detalle del producto

--- src/interfaces/productInterfaces.tsx
+++ src/interfaces/productInterfaces.tsx
@@ -1,10 +1,33 @@
-export interface SimpleProduct{
-    id: string, 
-    nombre: string, 
-    marca: string, 
-    urlFoto: string, 
+export interface SimpleProduct {
+    id: string,
+    nombre: string,
+    marca: string,
+    urlFoto: string,
     ciudad: string,
-    precio: number, 
-    seller: string, 
+    precio: number,
+    seller: string,
     rating: number
+}
+
+export interface FullProduct {
+    id: string,
+    rating: number,
+    name: string,
+    brand: string,
+    pictures: string[],
+    city: City,
+    price: number,
+    description: string,
+    seller: Seller
+}
+
+interface City {
+    id: string,
+    name: string
+}
+
+interface Seller {
+    id: string,
+    name: string,
+    logo: string
 }
